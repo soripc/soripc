1770dd08ec7fcb82a7034c235c3df296270c4068
factusolution <factusolution@gmail.com>
2023-01-19 11:05:30 -0500

    cambio acceso a ecommerce|avance guia remision transportista

--- modules/ApiPeruDev/Helpers/ServiceDispatch.php
+++ modules/ApiPeruDev/Helpers/ServiceDispatch.php
@@ -1,28 +1,80 @@
 <?php
 
-namespace Modules\ApiPeruDev\Services;
+namespace Modules\ApiPeruDev\Helpers;
 
-use App\Models\Tenant\Company;
 use Exception;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
-use Modules\ApiPeruDev\Helpers\Zip;
 
-class DispatchService
+class ServiceDispatch
 {
-    public function getToken($soap_username, $soap_password, $client_id, $client_secret)
+    protected $ruc;
+    protected $is_demo;
+    protected $soap_username;
+    protected $soap_password;
+    protected $client_id;
+    protected $client_secret;
+
+    public function setCredentials($ruc, $is_demo, $soap_username, $soap_password, $client_id, $client_secret)
+    {
+        $this->ruc = $ruc;
+        $this->is_demo = $is_demo;
+
+        if ($this->is_demo) {
+            $this->soap_username = "{$this->ruc}MODDATOS";
+            $this->soap_password = 'MODDATOS';
+            $this->client_id = DispatchEndpoints::CLIENT_ID_BETA;
+            $this->client_secret = DispatchEndpoints::CLIENT_SECRET_BETA;
+        } else {
+            $this->soap_username = $soap_username;
+            $this->soap_password = $soap_password;
+            $this->client_id = $client_id;
+            $this->client_secret = $client_secret;
+        }
+    }
+
+    private function getEndpointToken()
+    {
+        $url = ($this->is_demo) ? DispatchEndpoints::TOKEN_BETA : DispatchEndpoints::TOKEN;
+
+        return str_replace('{client_id}', $this->client_id, $url);
+    }
+
+    private function getEndpointSend($filename)
+    {
+        $url = ($this->is_demo) ? DispatchEndpoints::SEND_BETA : DispatchEndpoints::SEND;
+
+        return str_replace('{filename}', $filename, $url);
+    }
+
+    private function getEndpointTicket($numTicket)
+    {
+        $url = ($this->is_demo) ? DispatchEndpoints::TICKET_BETA : DispatchEndpoints::TICKET;
+
+        return str_replace('{numTicket}', $numTicket, $url);
+    }
+
+    public function getToken()
     {
         try {
-            if (is_null($soap_username) || $soap_username === '') {
+            if (Cache::has("{$this->ruc}_token_sunat")) {
+                return [
+                    'success' => true,
+                    'token' => Cache::get("{$this->ruc}_token_sunat"),
+                    'cache' => true
+                ];
+            }
+
+            if (is_null($this->soap_username) || $this->soap_username === '') {
                 throw new Exception('El Soap Username es requerido');
             }
-            if (is_null($soap_password) || $soap_password === '') {
+            if (is_null($this->soap_password) || $this->soap_password === '') {
                 throw new Exception('El Soap Password es requerido');
             }
-            if (is_null($client_id) || $client_id === '') {
+            if (is_null($this->client_id) || $this->client_id === '') {
                 throw new Exception('El Client ID es requerido');
             }
-            if (is_null($client_secret) || $client_secret === '') {
+            if (is_null($this->client_secret) || $this->client_secret === '') {
                 throw new Exception('El Client Secret es requerido');
             }
 
@@ -30,14 +82,14 @@ class DispatchService
             $form_params = [
                 'grant_type' => 'password',
                 'scope' => 'https://api-cpe.sunat.gob.pe',
-                'client_id' => $client_id,
-                'client_secret' => $client_secret,
-                'username' => $soap_username,
-                'password' => $soap_password,
+                'client_id' => $this->client_id,
+                'client_secret' => $this->client_secret,
+                'username' => $this->soap_username,
+                'password' => $this->soap_password,
             ];
 
             curl_setopt_array($curl, array(
-                CURLOPT_URL => "https://api-seguridad.sunat.gob.pe/v1/clientessol/{$client_id}/oauth2/token",
+                CURLOPT_URL => $this->getEndpointToken(),
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_ENCODING => '',
                 CURLOPT_MAXREDIRS => 10,
@@ -57,14 +109,10 @@ class DispatchService
             curl_close($curl);
 
             $data = json_decode($response, true);
-            Log::info('-----getToken------');
-            Log::info($data);
-            Log::info('-----getToken------');
 
             if (array_key_exists('access_token', $data)) {
                 $token = $data['access_token'];
-                Log::error('Cache toke_sunat actualizado');
-                Cache::put('token_sunat', $token, 60);
+                Cache::put("{$this->ruc}_token_sunat", $token, 60);
                 return [
                     'success' => true,
                     'token' => $token,
@@ -91,10 +139,10 @@ class DispatchService
         }
     }
 
-    public function send($soap_username, $soap_password, $client_id, $client_secret, $filename, $file_content)
+    public function send($filename, $file_content)
     {
         try {
-            $res = $this->getToken($soap_username, $soap_password, $client_id, $client_secret);
+            $res = $this->getToken();
             if (!$res['success']) {
                 throw new Exception($res['message']);
             }
@@ -109,7 +157,7 @@ class DispatchService
             ];
             $curl = curl_init();
             curl_setopt_array($curl, array(
-                CURLOPT_URL => "https://api-cpe.sunat.gob.pe/v1/contribuyente/gem/comprobantes/{$filename}",
+                CURLOPT_URL => $this->getEndpointSend($filename),
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_TIMEOUT => 2,
                 CURLOPT_FOLLOWLOCATION => true,
@@ -128,9 +176,6 @@ class DispatchService
             curl_close($curl);
 
             $res = json_decode($response, true);
-            Log::info('-----send------');
-            Log::info($res);
-            Log::info('-----send------');
 
             if (key_exists('cod', $res)) {
 
@@ -156,17 +201,17 @@ class DispatchService
         }
     }
 
-    public function ticket($soap_username, $soap_password, $client_id, $client_secret, $numTicket)
+    public function ticket($numTicket)
     {
         try {
-            $res = $this->getToken($soap_username, $soap_password, $client_id, $client_secret);
+            $res = $this->getToken();
             if (!$res['success']) {
                 throw new Exception($res['message']);
             }
             $token = $res['token'];
             $curl = curl_init();
             curl_setopt_array($curl, array(
-                CURLOPT_URL => "https://api-cpe.sunat.gob.pe/v1/contribuyente/gem/comprobantes/envios/{$numTicket}",
+                CURLOPT_URL => $this->getEndpointTicket($numTicket),
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_TIMEOUT => 2,
                 CURLOPT_FOLLOWLOCATION => true,
@@ -181,15 +226,10 @@ class DispatchService
             ));
             $response = curl_exec($curl);
             curl_close($curl);
-            $res = json_decode($response, true);
-            Log::info('-----ticket------');
-            Log::info($res);
-            Log::info('-----ticket------');
 
-            return $res;
+            return json_decode($response, true);
         } catch (Exception $e) {
             $message = "Code: {$e->getCode()} - Message: {$e->getMessage()}";
-            Log::info($message . ' ticket');
             return [
                 'success' => false,
                 'message' => $message
